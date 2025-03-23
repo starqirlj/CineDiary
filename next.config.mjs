a861b6ecb4b296b60dbb9c4b5a971628d91299c4
starqirlj <delossantosjasmine2@gmail.com>
2025-03-23 17:44:13 +0800

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
+};
+
+export default nextConfig;
